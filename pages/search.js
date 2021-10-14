7470478b927565c0c942a1ceb5fc31551bfb4595
pradow <pradochris+pradow@protonmail.com>
2021-10-13 18:16:07 -0700

    add functionality to fetch data from api

--- pages/search.js
+++ pages/search.js
@@ -1,8 +1,33 @@
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 function Search() {
   const router = useRouter();
-  return <h1>search {JSON.stringify(router.query)}</h1>;
+  const [fetchedData, setFetchedData] = useState([]);
+  let url = "https://pokeapi.co/api/v2/pokemon/";
+
+  async function fetchData(searchTerm) {
+    try {
+      const response = await fetch(url + searchTerm);
+      const data = await response.json();
+      console.log(data);
+      setFetchedData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    if (router.query.pokemon) {
+      fetchData(router.query.pokemon);
+    }
+  }, [router.query]);
+
+  if (fetchedData.length < 1) {
+    return <h1>no results</h1>;
+  }
+
+  return <h1>{JSON.stringify(fetchedData)}</h1>;
 }
 
 export default Search;
