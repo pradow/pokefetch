330185eecb4739ce97449fd63acb19a5ac887507
pradow <pradochris+pradow@protonmail.com>
2021-10-23 12:17:15 -0700

    add basepath to image

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import { useState } from "react";
 
@@ -46,7 +47,7 @@ export default function Home() {
               </div>
               <div className="hero-right">
                 <img
-                  src="/images/pokeball.png"
+                  src="/pokefetch/images/pokeball.png"
                   alt="Pokemon Pokeball"
                   className="hero-image"
                 />
