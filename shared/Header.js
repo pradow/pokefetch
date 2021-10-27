87e8e0b6d77226402d433e36c9277a8aaa6c0e1a
pradow <pradochris+pradow@protonmail.com>
2021-10-27 13:04:29 -0700

    add header design

--- shared/Header.js
+++ shared/Header.js
@@ -1,5 +1,17 @@
 function Header() {
-  return <h1>Header</h1>;
+  return (
+    <header className="main-header">
+      <div className="logo">
+        <a href="/pokefetch" className="logo-link" title="home">
+          <img
+            src="/pokefetch/images/pokeball.png"
+            alt="pokemon pokeball"
+            className="logo-image"
+          />
+        </a>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
