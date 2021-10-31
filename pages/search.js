c61fd0aae8908a1013372a1b1840631b799694ed
pradow <pradochris+pradow@protonmail.com>
2021-10-30 19:10:37 -0700

    start client side search filtering functionality

--- pages/search.js
+++ pages/search.js
@@ -1,22 +1,33 @@
 import { useRouter } from "next/router";
-import useFetch from "../shared/useFetch";
+import { pokemonNames } from "../shared/pokemonNames";
 
 function Search() {
   const router = useRouter();
-  let url = router.query.pokemon
-    ? `https://pokeapi.co/api/v2/pokemon/${router.query.pokemon}`
-    : null;
-  const { isLoading, data, isError } = useFetch(url);
+  const searchString = router.query.pokemon;
+  const matchArr = pokemonNames.filter((item) =>
+    item.name.includes(searchString)
+  );
 
-  if (isLoading) {
-    return <h2>...Loading</h2>;
-  }
+  const handleClick = (e) => {
+    const clickedPokemon = e.currentTarget.dataset.pokemon;
+    alert(clickedPokemon);
+  };
 
-  if (data.length < 1 || isError) {
+  if (matchArr < 1) {
     return <h1>no results</h1>;
   }
 
-  return <h1>{JSON.stringify(data)}</h1>;
+  return (
+    <div>
+      {matchArr.map((item) => {
+        return (
+          <h5 data-pokemon={item.name} onClick={handleClick}>
+            {item.name}
+          </h5>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Search;
