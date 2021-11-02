176c2491d10571620df7b3288d7aad16f6587487
pradow <pradochris+pradow@protonmail.com>
2021-11-02 09:47:24 -0700

    add functionality to extract pokemon id and add image elementto search results

--- pages/search.js
+++ pages/search.js
@@ -9,6 +9,8 @@ function Search() {
     item.name.includes(searchString)
   );
 
+  console.log(matchArr);
+
   if (matchArr < 1) {
     return <h1>no results</h1>;
   }
@@ -16,10 +18,18 @@ function Search() {
   return (
     <div>
       {matchArr.map((item, index) => {
+        const pokemonId = item.url
+          .slice(item.url.indexOf("/pokemon/"))
+          .slice(9, -1);
+
         return (
           <Link href={`/pokemon?name=${item.name}`}>
             <a>
               <h5>{item.name}</h5>
+              <img
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`}
+                alt=""
+              />
             </a>
           </Link>
         );
