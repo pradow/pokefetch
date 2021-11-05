ee6d1563f2bacd0062eeb6f1687e66790694a97e
pradow <pradochris+pradow@protonmail.com>
2021-11-05 09:43:39 -0700

    start displaying data for chosen category

--- pages/pokemon/CharacterInfo.js
+++ pages/pokemon/CharacterInfo.js
@@ -3,12 +3,73 @@ import useFetch from "../../shared/useFetch";
 
 function CharacterInfo({ data }) {
   const [category, SetCategory] = useState("");
+  const [fetchData, setFetchedData] = useState([]);
   const handleClick = (e) => {
-    alert(e.currentTarget.dataset.category);
+    SetCategory(e.currentTarget.dataset.category);
   };
 
-  if (category) {
-    return <div>{data.category}</div>;
+  if (category === "abilities") {
+    return (
+      <div>
+        {data[category].map((el) => {
+          return <h5>{el.ability.name}</h5>;
+        })}
+      </div>
+    );
+  }
+
+  if (category === "held_items") {
+    return (
+      <div>
+        {data[category].map((el) => {
+          return <h5>{el.item.name}</h5>;
+        })}
+      </div>
+    );
+  }
+
+  if (category === "moves") {
+    return (
+      <div>
+        {data[category].map((el) => {
+          return <h5>{el.move.name}</h5>;
+        })}
+      </div>
+    );
+  }
+
+  if (category === "stats") {
+    return (
+      <div>
+        {data[category].map((el) => {
+          return (
+            <h5>
+              {el.stat.name}
+              {el.base_stat}
+            </h5>
+          );
+        })}
+      </div>
+    );
+  }
+
+  if (category === "location_area_encounters") {
+    return <div>hi</div>;
+  }
+
+  if (category === "game_indices") {
+    return (
+      <div>
+        {data[category].map((el) => {
+          return (
+            <h5>
+              {el.game_index}
+              {el.version.name}
+            </h5>
+          );
+        })}
+      </div>
+    );
   }
 
   return (
