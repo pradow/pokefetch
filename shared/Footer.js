60f049a68a493bc0c1e6cf19de778a9333c7d536
pradow <pradochris+pradow@protonmail.com>
2021-11-11 13:24:56 -0800

    add design styles to character info section

--- shared/Footer.js
+++ shared/Footer.js
@@ -1,12 +1,14 @@
 function Footer() {
   return (
     <footer className="main-footer">
-      <a href="https://pradow.com/" className="footer-link">
-        @pradow ↗
-      </a>
-      <a href="https://pokeapi.co/" className="footer-link">
-        Data Used ↗
-      </a>
+      <div className="container">
+        <a href="https://pradow.com/" className="footer-link">
+          @pradow ↗
+        </a>
+        <a href="https://pokeapi.co/" className="footer-link">
+          Data Used ↗
+        </a>
+      </div>
     </footer>
   );
 }
